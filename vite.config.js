2ab71a48da4928c78cede6c4c080eba0fa1b1c03
Ram4GB <leminhcuong2988@yahoo.com>
2022-05-22 13:22:24 +0700

    config accepted extension & mode

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ export default defineConfig(({ command, mode }) => {
   console.log(mode, loadEnv(mode, process.cwd(), "VITE_"));
 
   return {
+    root: process.cwd(),
+    base: "/",
+    mode: mode === "development" ? "development" : "production",
+    define: {
+      __APP_ENV__: "ADMIN", // this is not working now
+    },
+    resolve: {
+      extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
+    },
     cacheDir: ".vite",
     publicDir: "public",
     resolve: {
