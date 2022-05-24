23c56ef10c449efd5f1238b9301b4b36cddb6078
Ram4GB <leminhcuong2988@yahoo.com>
2022-05-24 09:10:15 +0700

    fix service api

--- service/instance.js
+++ service/instance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/",
-  timeout: 500,
+  baseURL: "http://localhost:3000/",
+  timeout: 30000,
 });
 
 instance.interceptors.request.use(
